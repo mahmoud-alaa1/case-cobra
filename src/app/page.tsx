5d38fece8c8466417e25455775b4610e18f63525
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-24 08:58:57 +0300

    Refactor image imports and update Phone component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,7 +98,7 @@ export default function Home() {
                 alt=""
               />
               <Image src={line} className="absolute w-20 -left-6 -bottom-6 select-none" alt="" />
-              <Phone imgSrc={testimonials1} className="w-64" />
+              <Phone ImageSrc={testimonials1} className="w-64" />
             </div>
           </div>
         </MaxWidthWrapper>
@@ -128,7 +128,24 @@ export default function Home() {
                 <Star className="h-5 w-5 text-green-600 fill-green-600" />
                 <Star className="h-5 w-5 text-green-600 fill-green-600" />
               </div>
-              <div className="text-lg leading-8"><p>"The case feels durable and i even got a compliment on the design. Had the case for " </p></div>
+              <div className="text-lg leading-8">
+                <p>
+                  &quot;The case feels durable and I even got a compliment on the design. Had the case for two and a
+                  half months now and <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>,
+                  on the case I had before, the image started fading into yellow-ish color after a couple weeks. Love
+                  it.&quot;
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <Image className="rounded-full h-12 w-12 object-cover" src={userImage1} alt="user" />
+                <div className="flex flex-col ">
+                  <p className="font-semibold">Jonathan </p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 aspect-square stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -4,11 +4,11 @@ import React, { HTMLAttributes } from "react";
 import phoneTemplateDarkEdges from "../../public/phone-template-dark-edges.png";
 import phoneTemplateWhiteEdges from "../../public/phone-template-white-edges.png";
 interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
-  imgSrc: StaticImageData | string;
+  ImageSrc: StaticImageData | string;
   dark?: boolean;
 }
 
-export default function Phone({ imgSrc, dark = false, className, ...props }: PhoneProps) {
+export default function Phone({ ImageSrc, dark = false, className, ...props }: PhoneProps) {
   return (
     <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)} {...props}>
       <Image
@@ -17,7 +17,7 @@ export default function Phone({ imgSrc, dark = false, className, ...props }: Pho
         alt="phone image"
       />
       <div className="absolute -z-10 inset-0">
-        <Image className="object-cover" src={imgSrc} alt="overlaying phone image" />
+        <Image className="object-cover" src={ImageSrc} alt="overlaying phone image" />
       </div>
     </div>
   );
