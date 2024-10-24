323225053ef8beb60ff3b8013277d094d5838df7
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-24 17:13:25 +0300

    add Zod library for validation

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,12 +1,14 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import { z } from "zod";
 
 const f = createUploadthing();
 
 const auth = (req: Request) => ({ id: "fakeId" });
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } }).input(z.object)
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .input(z.object)
     .middleware(async ({ req }) => {
       const user = await auth(req);
 
