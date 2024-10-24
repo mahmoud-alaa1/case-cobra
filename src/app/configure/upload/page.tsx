65f63819e01194e49ffff0b901ee54616e2c8219
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-24 16:23:32 +0300

    Refactor upload page to adjust layout and add drag and drop functionality

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -12,13 +12,13 @@ export default function Page() {
   return (
     <div
       className={cn(
-        "relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",
+        "relative h-full flex-1 my-8 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",
         {
           "ring-blue-900/25 bg-blue-900/10": isDragOver,
         }
       )}
     >
-      <div className="relative flex flex-1 flex-col items-center justify-center w-full min-h-[60vh]">
+      <div className="relative flex flex-1 flex-col items-center justify-center w-full min-h-[50vh]">
         <Dropzone
           onDropRejected={onDropRejected}
           onDropAccepted={onDropAccepted}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <footer className="bg-white  relative">
+    <footer className="bg-white h-20 relative">
       <MaxWidthWrapper>
         <div className="border-t border-gray-200" />
+
         <div className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center">
-          <div className="text-center md:text-lg pb-2 md:pb-0">
+          <div className="text-center md:text-left pb-2 md:pb-0">
             <p className="text-sm text-muted-foreground">&copy; {new Date().getFullYear()} All rights reserved</p>
           </div>
+
           <div className="flex items-center justify-center">
             <div className="flex space-x-8">
               <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
@@ -27,4 +29,6 @@ export default function Footer() {
       </MaxWidthWrapper>
     </footer>
   );
-}
+};
+
+export default Footer;
