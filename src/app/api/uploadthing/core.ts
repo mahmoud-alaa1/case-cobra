37d2f80c3744cb4fa1f002e415489ef4b4288baf
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-24 23:46:22 +0300

    I have coded the server core.ts and i'm buildng the deign page

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
-
+import sharp from "sharp";
+import db from "@/db";
 const f = createUploadthing();
 
 export const ourFileRouter = {
@@ -12,8 +13,26 @@ export const ourFileRouter = {
     .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input;
       const res = await fetch(file.url);
-   
-      return { configId };
+      const buffer = await res.arrayBuffer();
+      const imageMetadata = await sharp(buffer).metadata();
+      const { height, width } = imageMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: { id: configId },
+          data: { croppedImageUrl: file.url },
+        });
+        return { configId: updatedConfiguration.id };
+      }
     }),
 } satisfies FileRouter;
 
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,28 @@
-export default async function Page() {
-  //make a db call
+import db from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+interface pageProps {
+  searchParams: {
+    [key: string]: string | string | undefined;
+  };
+}
+export default async function Page({ searchParams }: pageProps) {
+  const { id } = searchParams;
 
-  return <p>page</p>;
+  console.log("id", id);
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, height, width } = configuration;
+
+  return <DesignConfigurator configId={configuration.id} imageDimensions={{ height, width }} imageUrl={imageUrl} />;
 }
