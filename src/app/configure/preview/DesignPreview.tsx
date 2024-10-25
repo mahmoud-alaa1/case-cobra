0e32fd320a6b080526636c568822d3913db9c622
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-25 17:52:33 +0300

    include react-dom-confetti  to celebrate user for compeleting the order process

---
+++ src/app/configure/preview/DesignPreview.tsx
@@ -0,0 +1,12 @@
+export default function DesignPreview() {
+  return (
+    <>
+      <div
+        aria-hidden="true"
+        className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center"
+      >
+        
+      </div>
+    </>
+  );
+}
