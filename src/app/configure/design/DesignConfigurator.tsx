33fe51e7f34cfad3ad3f9abd09a998a049ee0119
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-25 11:11:21 +0300

    Refactor DesignConfigurator to include dropdown menu for selecting options and improve rendering functionality

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -21,10 +21,17 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Label } from "@/components/ui/label";
 import { BASE_PRICE } from "@/config/products";
-import { COLORS, FINISHES, MATERIALS, MODELS } from "@/validators/option-validator";
+import {
+  COLORS,
+  FINISHES,
+  MATERIALS,
+  MODELS,
+} from "@/validators/option-validator";
 import { ArrowRight, Check, ChevronsUpDown } from "lucide-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Rnd } from "react-rnd";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useToast } from "@/hooks/use-toast";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -32,7 +39,13 @@ interface DesignConfiguratorProps {
   imageDimensions: { width: number; height: number };
 }
 
-export default function DesignConfigurator({ configId, imageUrl, imageDimensions }: DesignConfiguratorProps) {
+export default function DesignConfigurator({
+  configId,
+  imageUrl,
+  imageDimensions,
+}: DesignConfiguratorProps) {
+  const { toast } = useToast();
+
   const [options, setOptions] = useState<{
     color: (typeof COLORS)[number];
     model: (typeof MODELS.options)[number];
@@ -45,17 +58,89 @@ export default function DesignConfigurator({ configId, imageUrl, imageDimensions
     finish: FINISHES.options[0],
   });
 
+  const phoneCaseRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
   const [renderedDimensions, setRenderedDimensions] = useState({
     width: imageDimensions.width / 4,
     height: imageDimensions.height / 4,
   });
   const [rendredPosition, setRenderedPosition] = useState({ x: 150, y: 205 });
 
+  const { startUpload, isUploading } = useUploadThing("imageUploader");
+
+  async function saveConfiguration() {
+    try {
+      const {
+        left: caseLeft,
+        top: caseTop,
+        width,
+        height,
+      } = phoneCaseRef.current!.getBoundingClientRect();
+      const { left: containerLeft, top: containerTop } =
+        containerRef.current!.getBoundingClientRect();
+      const leftOffset = caseLeft - containerLeft;
+      const topOffset = caseTop - containerTop;
+      const actualX = rendredPosition.x - leftOffset;
+      const actualY = rendredPosition.y - topOffset;
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext("2d");
+      const userImage = new Image();
+      userImage.crossOrigin = "anonymous";
+      userImage.src = imageUrl;
+      await new Promise((resolve) => {
+        userImage.onload = resolve;
+      });
+      ctx?.drawImage(
+        userImage,
+        actualX,
+        actualY,
+        renderedDimensions.width,
+        renderedDimensions.height
+      );
+      const base64 = canvas.toDataURL("image/png");
+      console.log(base64);
+      const base64Data = base64.split(",")[1];
+      const blob = base64ToBlob(base64Data, "image/png");
+      console.log(blob);
+      const file = new File([blob], "phone-case.png", { type: "image/png" });
+      await startUpload([file], { configId });
+    } catch (error) {
+      toast({
+        title: "Something went wrong",
+        description:
+          "There was a problem saving your config, please try again.",
+        variant: "destructive",
+      });
+    }
+  }
+
+  function base64ToBlob(base64: string, type: string) {
+    const byteCharacters = atob(base64);
+    console.log(byteCharacters);
+    const byteNumbers = new Array(byteCharacters.length);
+    console.log(byteNumbers);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type });
+  }
+
   return (
     <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
-      <div className="relative h-[37.5rem] overflow-hidden col-span-2 max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+      <div
+        ref={containerRef}
+        className="relative h-[37.5rem] overflow-hidden col-span-2 max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+      >
         <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
-          <AspectRatio ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831] w-full">
+          <AspectRatio
+            ref={phoneCaseRef}
+            ratio={896 / 1831}
+            className="pointer-events-none relative z-50 aspect-[896/1831] w-full"
+          >
             <div className="realtive">
               <NextImage
                 fill
@@ -100,7 +185,12 @@ export default function DesignConfigurator({ configId, imageUrl, imageDimensions
           }}
         >
           <div className="relative w-full h-full">
-            <NextImage src={imageUrl} alt="your image" className="pointer-events-none" fill />
+            <NextImage
+              src={imageUrl}
+              alt="your image"
+              className="pointer-events-none"
+              fill
+            />
           </div>
         </Rnd>
       </div>
@@ -111,7 +201,9 @@ export default function DesignConfigurator({ configId, imageUrl, imageDimensions
             className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"
           />
           <div className="px-8 pb-12 pt-8">
-            <h2 className="tracking-tight font-bold text-3xl">Customize your case</h2>
+            <h2 className="tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
             <div className="w-full h-px bg-zinc-200 my-6" />
             <div className="realtive  mt-4 h-full flex flex-col justify-between">
               <div className="flex flex-col gap-6">
@@ -156,7 +248,11 @@ export default function DesignConfigurator({ configId, imageUrl, imageDimensions
                   <Label>Model</Label>
                   <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                      <Button variant="outline" role="combobox" className="w-full justify-between">
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className="w-full justify-between"
+                      >
                         {options.model.label}
                         <ChevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
                       </Button>
@@ -168,15 +264,20 @@ export default function DesignConfigurator({ configId, imageUrl, imageDimensions
                           className={cn(
                             "flex text-sm gap-1 items-center p-1.5 cursor-default hover:bg-zinc-100 transition",
                             {
-                              "bg-zinc-100": model.value === options.model.value,
+                              "bg-zinc-100":
+                                model.value === options.model.value,
                             }
                           )}
-                          onClick={() => setOptions((prev) => ({ ...prev, model }))}
+                          onClick={() =>
+                            setOptions((prev) => ({ ...prev, model }))
+                          }
                         >
                           <Check
                             className={cn(
                               "mr-2 size-4",
-                              model.label === options.model.label ? "opacity-100" : "opacity-0"
+                              model.label === options.model.label
+                                ? "opacity-100"
+                                : "opacity-0"
                             )}
                           />
                           {model.label}
@@ -185,52 +286,66 @@ export default function DesignConfigurator({ configId, imageUrl, imageDimensions
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
-                {[MATERIALS, FINISHES].map(({ name, options: selectableOptions }) => (
-                  <RadioGroup
-                    key={name}
-                    value={options[name]}
-                    onChange={(val) => {
-                      setOptions((prev) => ({ ...prev, [name]: val }));
-                    }}
-                  >
-                    <Label>{name.slice(0, 1).toUpperCase() + name.slice(1)}</Label>
-                    <div className="mt-3 space-y-4">
-                      {selectableOptions.map((option) => (
-                        <RadioGroup.Option
-                          key={option.value}
-                          value={option}
-                          className={({ active, checked }) =>
-                            cn(
-                              "relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between",
-                              {
-                                "border-primary": checked || checked,
-                              }
-                            )
-                          }
-                        >
-                          <span className="flex items-center">
-                            <span className="flex flex-col text-sm">
-                              <RadioGroup.Label as="span" className="font-medium text-gray-900">
-                                {option.label}
-                              </RadioGroup.Label>
-                              {option.description && (
-                                <RadioGroup.Description as="span" className="text-gray-500">
-                                  <span className="block sm:inline">{option.description}</span>
-                                </RadioGroup.Description>
-                              )}
-                            </span>
-                          </span>
-                          <RadioGroup.Description
-                            as="span"
-                            className="mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right "
+                {[MATERIALS, FINISHES].map(
+                  ({ name, options: selectableOptions }) => (
+                    <RadioGroup
+                      key={name}
+                      value={options[name]}
+                      onChange={(val) => {
+                        setOptions((prev) => ({ ...prev, [name]: val }));
+                      }}
+                    >
+                      <Label>
+                        {name.slice(0, 1).toUpperCase() + name.slice(1)}
+                      </Label>
+                      <div className="mt-3 space-y-4">
+                        {selectableOptions.map((option) => (
+                          <RadioGroup.Option
+                            key={option.value}
+                            value={option}
+                            className={({ active, checked }) =>
+                              cn(
+                                "relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between",
+                                {
+                                  "border-primary": checked || checked,
+                                }
+                              )
+                            }
                           >
-                            <span className="font-medium text-gray-900">{formatPrice(option.price / 100)}</span>
-                          </RadioGroup.Description>
-                        </RadioGroup.Option>
-                      ))}
-                    </div>
-                  </RadioGroup>
-                ))}
+                            <span className="flex items-center">
+                              <span className="flex flex-col text-sm">
+                                <RadioGroup.Label
+                                  as="span"
+                                  className="font-medium text-gray-900"
+                                >
+                                  {option.label}
+                                </RadioGroup.Label>
+                                {option.description && (
+                                  <RadioGroup.Description
+                                    as="span"
+                                    className="text-gray-500"
+                                  >
+                                    <span className="block sm:inline">
+                                      {option.description}
+                                    </span>
+                                  </RadioGroup.Description>
+                                )}
+                              </span>
+                            </span>
+                            <RadioGroup.Description
+                              as="span"
+                              className="mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right "
+                            >
+                              <span className="font-medium text-gray-900">
+                                {formatPrice(option.price / 100)}
+                              </span>
+                            </RadioGroup.Description>
+                          </RadioGroup.Option>
+                        ))}
+                      </div>
+                    </RadioGroup>
+                  )
+                )}
               </div>
             </div>
           </div>
@@ -240,9 +355,12 @@ export default function DesignConfigurator({ configId, imageUrl, imageDimensions
           <div className="w-full h-full flex justify-end items-center">
             <div className="w-full flex gap-6 items-center">
               <p className="font-medium whitespace-nowrap">
-                {formatPrice((BASE_PRICE + options.finish.price + options.material.price) / 100)}
+                {formatPrice(
+                  (BASE_PRICE + options.finish.price + options.material.price) /
+                    100
+                )}
               </p>
-              <Button size="sm" className="w-full">
+              <Button onClick={saveConfiguration} size="sm" className="w-full">
                 Continue <ArrowRight className="size-4 ml-1.5 inline" />
               </Button>
             </div>
