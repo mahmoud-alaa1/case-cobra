48a0c795981b19220c40b64138c9a991e8569745
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-26 21:31:15 +0300

    Refactor ThankYou component to use "id" instead of "orderId" in searchParams

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -9,7 +9,7 @@ import { formatPrice } from "@/lib/utils";
 
 const ThankYou = () => {
   const searchParams = useSearchParams();
-  const orderId = searchParams.get("orderId") || "";
+  const orderId = searchParams.get("id") || "";
 
   const { data } = useQuery({
     queryKey: ["get-payment-status"],
