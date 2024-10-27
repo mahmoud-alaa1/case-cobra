cb919755797b2aa57311ecc9550fb91adf9fc3b3
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-27 20:53:26 +0300

    Refactor dashboard page component to display revenue statistics and incoming orders

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,14 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import db from "@/db";
+import { formatPrice } from "@/lib/utils";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound } from "next/navigation";
 
@@ -28,7 +38,7 @@ export default async function Page() {
       shippingAddress: true,
     },
   });
-  const lastWeekRevenue = db.order.aggregate({
+  const lastWeekRevenue = await db.order.aggregate({
     where: {
       isPaid: true,
       createdAt: {
@@ -42,14 +52,73 @@ export default async function Page() {
       amount: true,
     },
   });
+  const lastMonthRevenue = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 30)), //last 30 days
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const WEEKLY_GOAL = 500;
+  const MONTHLY_GOAL = 2500;
 
   return (
     <div className="flex min-h-screen w-full bg-muted/40">
       <div className="max-w-7xl w-full mx-auto flex flex-col sm:gap-4 sm:py-4 ">
         <div className="flex flex-col gap-16">
           <div className="grid gap-4 sm:grid-cols-2">
-            <Card />
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last Week</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastWeekRevenue._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(WEEKLY_GOAL)} goal
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress
+                  value={Math.floor(
+                    ((lastWeekRevenue._sum.amount ?? 0) / WEEKLY_GOAL) * 100
+                  )}
+                />
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last Month</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastMonthRevenue._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(MONTHLY_GOAL)} goal
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress
+                  value={Math.floor(
+                    ((lastMonthRevenue._sum.amount ?? 0) / MONTHLY_GOAL) * 100
+                  )}
+                />
+              </CardFooter>
+            </Card>
           </div>
+          <h1 className="text-4xl font-bold tracking-tight">
+            Incoming Orders
+          </h1>
         </div>
       </div>
     </div>
