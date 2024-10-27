8cbd23649f533b80389df0b01009a4a47d93fe1f
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-27 20:41:13 +0300

    Refactor dashboard page component to use flex layout and set background color

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,57 @@
-export default function Page() {
-  return <div className="flex min-h-screen w-full bg-muted/40">page</div>;
+import db from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound } from "next/navigation";
+
+export default async function Page() {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+
+  if (!user || user.email !== ADMIN_EMAIL) {
+    return notFound(); //so make them think it doesn't exist
+  }
+
+  const orders = await db.order.findMany({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)), //last 7 days
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      user: true,
+      shippingAddress: true,
+    },
+  });
+  const lastWeekRevenue = db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)), //last 7 days
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  return (
+    <div className="flex min-h-screen w-full bg-muted/40">
+      <div className="max-w-7xl w-full mx-auto flex flex-col sm:gap-4 sm:py-4 ">
+        <div className="flex flex-col gap-16">
+          <div className="grid gap-4 sm:grid-cols-2">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
