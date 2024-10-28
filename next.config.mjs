38d0419cf5a87ebe37c8b132a67df2402d37f2c3
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-28 19:14:37 +0300

    include i18n configuration to enable multilanguages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
+  i18n: {
+    locales: ["en", "ar"], // Add your supported locales
+    defaultLocale: "en", // Set the default locale
+  },
 };
 
 export default nextConfig;
