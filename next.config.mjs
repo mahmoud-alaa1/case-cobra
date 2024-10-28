9b655d3f7ae673a9fa87661c67c405f481a7a29a
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-28 21:21:40 +0300

    Refactor next.config.mjs to remove i18n configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
-  i18n: {
-    locales: ["en", "ar"], // Add your supported locales
-    defaultLocale: "en", // Set the default locale
-  },
+
 };
 
 export default nextConfig;
