88205ef13ef4848d74c9200f97846f83aa86b7dd
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-28 19:44:02 +0300

    Refactor Providers component to include KindeProvider for authentication

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,13 @@
 "use client";
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const client = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <KindeProvider>{children}</KindeProvider>
+    </QueryClientProvider>
+  );
 }
