47958718af5f9e26f91f678cc97401eb75c2b907
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-28 16:57:34 +0300

    Refactor metadata construction in layout.tsx and utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
+import image from "next/image";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,3 +15,37 @@ export const formatPrice = (price: number) => {
 
   return formatter.format(price);
 };
+
+export function constructMetaData({
+  title = "CaseCobra - custom high quality phone cases",
+  description = "Create custom high-quality phone cases in seconds.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+          alt: title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+    },
+    icons,
+  };
+}
